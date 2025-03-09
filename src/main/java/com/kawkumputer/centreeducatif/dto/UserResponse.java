ef5c7cb7b21cd60b12a4a8b5c655be6923f03a2e
Hamath Kane <kawkumputer@gmail.com>
2025-03-09 11:48:02 +0100

    add gitignore

--- src/main/java/com/kawkumputer/centreeducatif/dto/UserResponse.java
+++ src/main/java/com/kawkumputer/centreeducatif/dto/UserResponse.java
@@ -1,11 +1,15 @@
 package com.kawkumputer.centreeducatif.dto;
 
 import com.kawkumputer.centreeducatif.domain.Role;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponse {
     private Long id;
     private String firstName;
